4bb96c43c4dfcb876b3af085a15bf64d85703e01
Natthinee Wongsavit <nwongsavit@chirotouch.com>
2019-02-07 13:43:26 -0800

    Added my Hello World program

---
+++ Assignment1/wongsavit.cpp
@@ -0,0 +1,14 @@
+/*
+Natthinee Wongsavit
+Andreas Stefik
+CS 472 1001
+Git Assignment - Hello World Program
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World" << endl;
+  return 0;
+}
