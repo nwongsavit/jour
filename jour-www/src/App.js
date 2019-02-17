dd52a5527e40042de70b59feae287f61acc1b8f2
Natthinee Wongsavit <nwongsavit@chirotouch.com>
2019-02-17 01:43:20 -0800

    added navbar component and routes

--- jour-www/src/App.js
+++ jour-www/src/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPencilAlt, faHome, faCalendar, faChartBar, faCog, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import NavBar from './components/navBar';
+import HomePage from "./components/Page/homePage";
+import CalendarPage from "./components/Page/calendarPage";
+import StatsPage from "./components/Page/statisticsPage";
+import SettingsPage from "./components/Page/settingsPage";
+import LoginPage from "./components/Page/loginPage";
 import './App.css';
+
+library.add(faPencilAlt, faHome, faCalendar, faChartBar, faCog, faSignOutAlt);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <NavBar/>
+        <div className="container">
+          <Switch>
+            <Route path="/home" component={HomePage} />
+            <Route path="/calendar" component={CalendarPage} />
+            <Route path="/stats" component={StatsPage} />
+            <Route path="/settings" component={SettingsPage} />
+            <Route path="/login" component={LoginPage} />
+            <Redirect path="/" exact to="/home" />
+          </Switch>
+        </div>
+      </React.Fragment>
     );
   }
 }
