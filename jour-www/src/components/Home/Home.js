2f040cdbc1ec57385e0b72576b62e5a62dd1af3d
Peter Hernandez <hernandez.peter998@gmail.com>
2019-02-21 17:57:56 -0800

    Added Textbox to Home Page

--- jour-www/src/components/Home/Home.js
+++ jour-www/src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Home.css';
+import Textbox2 from '../Textbox/Textbox2';
 
 class Home extends Component {
   render() {
     return (
       <div className="Home">
         Home
+      <Textbox2 />
       </div>
     );
   }
--- jour-www/src/components/Textbox/Textbox2.js
+++ jour-www/src/components/Textbox/Textbox2.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
-//import './Calendar.css';
+import './Textbox2.css';
 
 class Textbox2 extends Component {
   render() {
     return (
       <div className='Textbox2'>
-        Textbox
+      <p>How are you feeling today? </p>
+
+        <textarea rows="6" cols="100">
+      Respond to the question. Or write what's on your mind
 
 
+        </textarea>
+
+      <p>I am feeling: </p>
+
+      <select name="emotion" form="emotion">
+        <option value="happy">Happy</option>
+        <option value="sad">Sad</option>
+        <option value="angry">Angry</option>
+        <option value="anxious">Anxious</option>
+        <option value="confident">Confident</option>
+        <option value="nostalgic">Nostalgic</option>
+
+      </select>
+      <input type="Submit">
+      </input>
       </div>
     );
   }
