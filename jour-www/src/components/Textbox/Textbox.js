cdcb47dfd592578fb28bb2ebf40042a0137f7b60
Kassie <ksiuwng@gmail.com>
2019-02-27 21:43:10 -0800

    refactored

--- jour-www/src/components/Textbox/Textbox.js
+++ jour-www/src/components/Textbox/Textbox.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
 import './Textbox.css';
 
 class Textbox extends Component {
   render() {
     return (
       <div className="Textbox">
-        <p>How are you feeling today? </p>
+        <Form className="textboxForm">
+          <h3>How are you feeling today? </h3>
 
-
-        <p>I am feeling: </p>
-
-        <select name="emotion" form="emotion">
-          <option value="happy">Happy</option>
-          <option value="sad">Sad</option>
-          <option value="angry">Angry</option>
-          <option value="anxious">Anxious</option>
-          <option value="confident">Confident</option>
-          <option value="nostalgic">Nostalgic</option>
-
-        </select>
-        <input type="Submit" />
+          <Form.Control as="textarea" rows="3" />
+          <p>I am feeling: </p>
+          <Form.Control as="select">
+            <option value="happy">Happy</option>
+            <option value="sad">Sad</option>
+            <option value="angry">Angry</option>
+            <option value="anxious">Anxious</option>
+            <option value="confident">Confident</option>
+            <option value="nostalgic">Nostalgic</option>
+          </Form.Control>
+          <Button type="submit" block>
+            Submit
+          </Button>
+        </Form>
       </div>
     );
   }
