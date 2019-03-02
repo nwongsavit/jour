facf658090d14d280ab6200f12d9186ba6c5d624
nnorris25 <nnorris25@gmail.com>
2019-03-02 04:53:26 +0000

    switched string result to bool result.

--- jour-www/api/api.php
+++ jour-www/api/api.php
@@ -11,7 +11,7 @@ include('journal.php');
 
 //make sure link is set, if not send false result.
 if (!isset($link)) {
-    $error = array('result' => 'false', 'message' => "Error: couldn't get the database information.");
+    $error = array('result' => false, 'message' => "Error: couldn't get the database information.");
     echo json_encode($error);
     return;
 }
@@ -19,7 +19,7 @@ if (!isset($link)) {
 //check to make sure the api key is a valid key. currently only one key, but this can be expanded in the future
 //$api_key is defined in config.php
 if (!isset($_GET['key']) || $_GET['key'] != $api_key) {
-    $error = array('result' => 'false', 'message' => "Error: API key is missing on invaliid.");
+    $error = array('result' => false, 'message' => "Error: API key is missing on invaliid.");
     echo json_encode($error);
     return;
 }
@@ -27,7 +27,7 @@ if (!isset($_GET['key']) || $_GET['key'] != $api_key) {
 
 //error if no request is made and send the result.
 if (!isset($_GET['request'])) {
-    $error = array('result' => 'false', 'message' => "Error: Request required.");
+    $error = array('result' => false, 'message' => "Error: Request required.");
     echo json_encode($error);
     return;
 }
@@ -41,19 +41,19 @@ if (!isset($_GET['request'])) {
 if (isset($_GET['request']) && $_GET['request'] == "isUser") {
     //make sure api call requirements are met.
     if (!isset($_GET['email'])) {
-        $error = array('result' => 'false', 'message' => "Error: Missing required data. Please provide an email address.");
+        $error = array('result' => false, 'message' => "Error: Missing required data. Please provide an email address.");
         echo json_encode($error);
         return;
     }
     //we have what we need to see if the user already exists, make the check.
     $user = new users();
     if (!$user->isUser($_GET['email'])) {
-        $result = array('result' => 'false', 'message' => "No associated user.");
+        $result = array('result' => false, 'message' => "No associated user.");
         echo json_encode($result);
         return;
     }
     else {
-        $result = array('result' => 'true', 'message' => "An account with email address: ". $_GET['email'] . " already exists.");
+        $result = array('result' => true, 'message' => "An account with email address: ". $_GET['email'] . " already exists.");
         echo json_encode($result);
         return;
     }
@@ -62,19 +62,19 @@ if (isset($_GET['request']) && $_GET['request'] == "isUser") {
 if (isset($_GET['request']) && $_GET['request'] == "isWaitingConf") {
     //make sure api call requirements are met.
     if (!isset($_GET['email'])) {
-        $error = array('result' => 'false', 'message' => "Error: Missing required data. Please provide an email address.");
+        $error = array('result' => false, 'message' => "Error: Missing required data. Please provide an email address.");
         echo json_encode($error);
         return;
     }
     //we have what we need to see if the user already exists, make the check.
     $user = new users();
     if (!$user->isWaitingConf($_GET['email'])) {
-        $result = array('result' => 'false', 'message' => "No associated email is waiting confirmation.");
+        $result = array('result' => false, 'message' => "No associated email is waiting confirmation.");
         echo json_encode($result);
         return;
     }
     else {
-        $result = array('result' => 'true', 'message' => "An account with email address: ". $_GET['email'] . " is waiting for confirmation.");
+        $result = array('result' => true, 'message' => "An account with email address: ". $_GET['email'] . " is waiting for confirmation.");
         echo json_encode($result);
         return;
     }
@@ -93,13 +93,13 @@ if (isset($_GET['request']) && $_GET['request'] == "registerUser") {
         if (!isset($_GET['password'])) {
             $neededParams[] = "Password";
         }
-        $error = array('result' => 'false', 'message' => "Error: Missing required data. Please provide an email address, name, and password.", 'needed' => $neededParams);
+        $error = array('result' => false, 'message' => "Error: Missing required data. Please provide an email address, name, and password.", 'needed' => $neededParams);
         echo json_encode($error);
         return;
     }
     //check to make sure the data is valid.
     if (!filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) {
-        $error = array('result' => 'false', 'message' => "Error: The email address provided is not valid.");
+        $error = array('result' => false, 'message' => "Error: The email address provided is not valid.");
         echo json_encode($error);
         return;
     }
@@ -113,7 +113,7 @@ if (isset($_GET['request']) && $_GET['request'] == "registerUser") {
             1 number
             1 symbol (@#-_$%^&+=!?* space)
         */
-        $error = array('result' => 'false', 'message' => "Error: The password provided doesn't meet requirements.");
+        $error = array('result' => false, 'message' => "Error: The password provided doesn't meet requirements.");
         echo json_encode($error);
         return;
     }
@@ -121,23 +121,23 @@ if (isset($_GET['request']) && $_GET['request'] == "registerUser") {
     $user = new users();
     //lets double check and make sure that this email address isn't already taken.
     if ($user->isUser($_GET['email'])) {
-        $error = array('result' => 'false', 'message' => "Error: The email address provided is already associated with an account.");
+        $error = array('result' => false, 'message' => "Error: The email address provided is already associated with an account.");
         echo json_encode($error);
         return;
     }
     //check to make sure the user isn't already in the queue for confirmation
     if($user->isWaitingConf($_GET['email'])){
-        $error = array('result' => 'false', 'message' => "This email is already awaiting confirmation.");
+        $error = array('result' => false, 'message' => "This email is already awaiting confirmation.");
         echo json_encode($error);
         return;
     }
     if (!$user->registerUser($_GET['email'], $_GET['name'], $_GET['password'])) {
-        $error = array('result' => 'false', 'message' => "The account was not created.");
+        $error = array('result' => false, 'message' => "The account was not created.");
         echo json_encode($error);
         return;
     }
     else {
-        $result = array('result' => 'true', 'message' => "Account creation for " . $_GET['email'] . " is pending confirmation.");
+        $result = array('result' => true, 'message' => "Account creation for " . $_GET['email'] . " is pending confirmation.");
         echo json_encode($result);
         return;
     }
@@ -153,7 +153,7 @@ if (isset($_GET['request']) && $_GET['request'] == "confirmEmail") {
         if (!isset($_GET['emailKey'])) {
             $neededParams[] = "emailKey";
         }
-        $error = array('result' => 'false', 'message' => "Error: Missing required data. Please provide an email address and key", 'needed' => $neededParams);
+        $error = array('result' => false, 'message' => "Error: Missing required data. Please provide an email address and key", 'needed' => $neededParams);
         echo json_encode($error);
         return;
     }
@@ -161,18 +161,18 @@ if (isset($_GET['request']) && $_GET['request'] == "confirmEmail") {
     $user = new users();
     //first lets make sure that there is a need to confirm
     if(!$user->isWaitingConf($_GET['email'])){
-        $error = array('result' => 'false', 'message' => "This email is not in the confirmation queue.");
+        $error = array('result' => false, 'message' => "This email is not in the confirmation queue.");
         echo json_encode($error);
         return;
     }
     //there is an email match waiting, try to confirm.
     if (!$user->confirmEmail($_GET['email'], $_GET['emailKey'])) {
-        $result = array('result' => 'false', 'message' => "Unable to confirm the email address.");
+        $result = array('result' => false, 'message' => "Unable to confirm the email address.");
         echo json_encode($result);
         return;
     }
     else {
-        $result = array('result' => 'true', 'message' => "The account with email address: ". $_GET['email'] . " has been confirmed.");
+        $result = array('result' => true, 'message' => "The account with email address: ". $_GET['email'] . " has been confirmed.");
         echo json_encode($result);
         return;
     }
@@ -188,7 +188,7 @@ if (isset($_GET['request']) && $_GET['request'] == "authUser") {
         if (!isset($_GET['Password'])) {
             $neededParams[] = "Password";
         }
-        $error = array('result' => 'false', 'message' => "Error: Missing required data. Please provide an email address and password", 'needed' => $neededParams);
+        $error = array('result' => false, 'message' => "Error: Missing required data. Please provide an email address and password", 'needed' => $neededParams);
         echo json_encode($error);
         return;
     }
@@ -197,19 +197,19 @@ if (isset($_GET['request']) && $_GET['request'] == "authUser") {
     //first lets make sure that there is a need to authorize
     //check to see if the email has been confirmed
     if($user->isWaitingConf($_GET['email'])){
-        $error = array('result' => 'false', 'message' => "This email address still needs to be confirmed.");
+        $error = array('result' => false, 'message' => "This email address still needs to be confirmed.");
         echo json_encode($error);
         return;
     }
     //check to see if the user is in the database
     if(!$user->isUser($_GET['email'])){
-        $error = array('result' => 'false', 'message' => "This email is not associated with a valid account.");
+        $error = array('result' => false, 'message' => "This email is not associated with a valid account.");
         echo json_encode($error);
         return;
     }
     //there is a user with that email, and that email has been confirmed, try to authorize
     if (!$user->authUser($_GET['email'], $_GET['password'])) {
-        $result = array('result' => 'false', 'message' => "Login failed: Email/Password mismatch.");
+        $result = array('result' => false, 'message' => "Login failed: Email/Password mismatch.");
         echo json_encode($result);
         return;
     }
@@ -219,7 +219,7 @@ if (isset($_GET['request']) && $_GET['request'] == "authUser") {
         $account['joinDate'] = $user->getJoinDate();
         $account['email'] = $user->getEmail();
         $account['authKey'] = $user->getAuthKey();
-        $result = array('result' => 'true', 'message' => "Success.", 'account_info' => $account);
+        $result = array('result' => true, 'message' => "Success.", 'account_info' => $account);
         echo json_encode($result);
         return;
     }
@@ -245,18 +245,17 @@ if ($_GET['request'] == "addJournal") {
         if (!isset($_GET['authKey'])) {
             $neededParams[] = "authKey";
         }
-        $error = array('result' => 'false', 'message' => "Error: Missing required data. Please provide an email address and password", 'needed' => $neededParams);
+        $error = array('result' => false, 'message' => "Error: Missing required data. Please provide an email address and password", 'needed' => $neededParams);
         echo json_encode($error);
         return;
     }
-
     //all required params have been provided.
     //auth the user...
     //check the auth key
     $user = new users();
     if(!$user->confirmAuthKey($_GET['uid'],$_GET['authKey'])) {
 
-        $error = array('result' => 'false', 'message' => "Unable to authenticate user.");
+        $error = array('result' => false, 'message' => "Unable to authenticate user.");
         echo json_encode($error);
         return;
 
@@ -267,7 +266,7 @@ if ($_GET['request'] == "addJournal") {
     //the user passed auth, check journal length
     if (!$journal->checkJournalLength($_GET['journal'])) {
 
-        $error = array('result' => 'false', 'message' => "Journal length not valid.");
+        $error = array('result' => false, 'message' => "Journal length not valid.");
         echo json_encode($error);
         return;
 
@@ -275,7 +274,7 @@ if ($_GET['request'] == "addJournal") {
     //check the mood requirements...
     if (!$journal->checkMood($_GET['mood'])) {
 
-        $error = array('result' => 'false', 'message' => "invalid mood.");
+        $error = array('result' => false, 'message' => "invalid mood.");
         echo json_encode($error);
         return;
 
@@ -283,14 +282,14 @@ if ($_GET['request'] == "addJournal") {
     //every thing checks out, add the journal.
     if (!$journal->addJournal($_GET['uid'], $_GET['journal'],$_GET['mood'], $_GET['authKey'])) {
 
-        $error = array('result' => 'false', 'message' => "Unable to add journal entry.");
+        $error = array('result' => false, 'message' => "Unable to add journal entry.");
         echo json_encode($error);
         return;
 
     }
     else {
 
-        $result = array('result' => 'true', 'message' => "Success. Journal Added.");
+        $result = array('result' => true, 'message' => "Success. Journal Added.");
         echo json_encode($result);
         return;
 
