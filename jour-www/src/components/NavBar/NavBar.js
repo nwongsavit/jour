23ab5d501c585840e021a999eb5f14ef2c0452b9
adrian-alberto <woftles@gmail.com>
2019-03-13 18:51:47 -0700

    Basic Colors

--- jour-www/src/components/NavBar/NavBar.js
+++ jour-www/src/components/NavBar/NavBar.js
@@ -9,8 +9,8 @@ class NavBar extends Component {
       <div className="NavBar">
         <div className="container-fluid h-100">
           <div className="row h-100">
-            <aside className="col-12 col-md-2 p-0 bg-dark fixed-bottom">
-              <nav className="navbar navbar-expand navbar-dark bg-dark flex-md-column flex-row align-items-start py-2">
+            <aside className="col-12 col-md-2 p-0 css-navbar-bg fixed-bottom">
+              <nav className="navbar navbar-expand navbar-dark css-navbar-bg flex-md-column flex-row align-items-start py-2">
                 <div className="collapse navbar-collapse align-items-start">
                   <ul className="flex-md-column flex-row navbar-nav w-100 justify-content-between">
                     <li className="nav-item">
