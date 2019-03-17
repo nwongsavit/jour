1762c157d512c1ad6dbe3b42119501e41f3c38da
Kassie <ksiuwng@gmail.com>
2019-03-17 10:37:54 -0700

    working state

--- jour-www/src/components/NavBar/NavBar.js
+++ jour-www/src/components/NavBar/NavBar.js
@@ -6,61 +6,66 @@ import './NavBar.css';
 const bg = require('../../assets/jourlogo200w.png');
 
 class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: window.innerWidth,
+    };
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  }
+
   render() {
+    const { width } = this.state;
+    const isMobile = width <= 767;
+
+    let navClasses;
+    if (isMobile) {
+      navClasses = 'fixed-bottom';
+    } else {
+      navClasses = 'col-10 col-md-2';
+    }
     return (
-      <div className="NavBar">
-        <div className="container-fluid h-100">
-          <div className="row h-100">
-            <aside className="col-12 col-md-2 p-0 css-navbar-bg fixed-bottom">
-              <nav className="navbar navbar-expand navbar-dark css-navbar-bg flex-md-column flex-row align-items-start py-2">
-                <div className="collapse navbar-collapse align-items-start">
-                  <ul className="flex-md-column flex-row navbar-nav w-100 justify-content-between">
-                    <li className="nav-item">
-                      <Link className="navbar-brand pl-0 text-nowrap" to="/home">
-                        <img height="20em" src={bg} alt="Jour" />
-                      </Link>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link pl-0" to="/home">
-                        <FontAwesomeIcon icon="home" />
-                        &nbsp;&nbsp;
-                        <span className="d-none d-md-inline">Home</span>
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link pl-0" to="/calendar">
-                        <FontAwesomeIcon icon="calendar" />
-                        &nbsp;&nbsp;
-                        <span className="d-none d-md-inline">Calendar</span>
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link pl-0" to="/statistics">
-                        <FontAwesomeIcon icon="chart-bar" />
-                        &nbsp;&nbsp;
-                        <span className="d-none d-md-inline">Statistics</span>
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link pl-0" to="/settings">
-                        <FontAwesomeIcon icon="cog" />
-                        &nbsp;&nbsp;
-                        <span className="d-none d-md-inline">Settings</span>
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link pl-0" to="/login">
-                        <FontAwesomeIcon icon="sign-out-alt" />
-                        &nbsp;&nbsp;
-                        <span className="d-none d-md-inline">Sign out</span>
-                      </NavLink>
-                    </li>
-                  </ul>
-                </div>
-              </nav>
-            </aside>
+      <div className={`${navClasses} NavBar`}>
+        <nav className="navbar navbar-expand navbar-dark">
+          <div className="navbar-collapse">
+            <ul className="flex-md-column navbar-nav w-100 test">
+              <Link id="logo" to="/home">
+                <img height="30em" src={bg} alt="Jour" />
+              </Link>
+              <NavLink className="nav-item nav-link" activeClassName="active" to="/home">
+                <FontAwesomeIcon className="icon" icon="home" />
+                <span className="d-none d-md-inline">Home</span>
+              </NavLink>
+              <NavLink className="nav-item nav-link" activeClassName="active" to="/calendar">
+                <FontAwesomeIcon className="icon" icon="calendar" />
+                <span className="d-none d-md-inline">Calendar</span>
+              </NavLink>
+              <NavLink className="nav-item nav-link" activeClassName="active" to="/statistics">
+                <FontAwesomeIcon className="icon" icon="chart-bar" />
+                <span className="d-none d-md-inline">Statistics</span>
+              </NavLink>
+              <NavLink className="nav-item nav-link" activeClassName="active" to="/settings">
+                <FontAwesomeIcon className="icon" icon="cog" />
+                <span className="d-none d-md-inline">Settings</span>
+              </NavLink>
+              <NavLink className="nav-item nav-link" activeClassName="active" to="/login">
+                <FontAwesomeIcon className="icon" icon="sign-out-alt" />
+                <span className="d-none d-md-inline">Sign out</span>
+              </NavLink>
+            </ul>
           </div>
-        </div>
+        </nav>
       </div>
     );
   }
