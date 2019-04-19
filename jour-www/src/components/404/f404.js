098f8c54c011ee3c16a3746db9c09b95a2e2aae4
Peter Hernandez <hernandez.peter998@gmail.com>
2019-04-18 17:14:47 -0700

    Created a 404 Page. May or may not need more (a graphic?)

---
+++ jour-www/src/components/404/f404.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './f404.css';
+
+class f404 extends Component{
+  render() {
+    return (
+      <div className="f404">
+
+      <h2>Oh hey! Didn't expect you here</h2>
+
+      <h3> What you are looking for is not here </h3>
+      <h3> But it may be on the toolbar on the side! </h3>
+
+      <a href="https://jour.life/home">Go Home</a>
+
+      </div>
+    ) ;
+  }
+
+
+
+}
+
+export default f404;
