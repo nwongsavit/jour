9fecf0e08d161975998f8c272c3d9d2d96c4e608
Kyler Earhart <earhart@unlv.nevada.edu>
2019-04-26 22:18:45 -0700

    adding ForgotPassword.js

--- jour-www/src/components/User/ForgotPassword.js
+++ jour-www/src/components/User/ForgotPassword.js
@@ -6,6 +6,22 @@ class ForgotPassword extends Component {
     return (
       <div className="ForgotPassword">
         Forgot Password
+      <head>
+        Forgot Your Password?
+      </head>
+      <body>
+        <h3>Please enter your email.</h3>
+
+            <div class="field-wrap">
+                <label>
+                    Email<span class="req">*</span>
+                </label>
+                <input type="email" required autocomplete="off"/>
+            </div>
+
+            <button type="submit" class="button button-block">Search</button>
+      </body>
+
       </div>
     );
   }
