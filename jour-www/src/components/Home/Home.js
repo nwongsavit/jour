8dc62c5bbc173cdcd448cb889976f483f9c15b60
adrian-alberto <woftles@gmail.com>
2019-05-02 13:36:48 -0700

    Home page title

--- jour-www/src/components/Home/Home.js
+++ jour-www/src/components/Home/Home.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import './Home.css';
 import Textbox from '../Textbox/Textbox';
 
+
+
 class Home extends Component {
   render() {
+    componentDidMount: function() {
+      document.title = "Jour - Home";
+    };
     return (
       <div className="Home">
         <Textbox />
