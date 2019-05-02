aa1fd2638c5b8c9ef706add776ff1631150f00c7
Peter Hernandez <hernandez.peter998@gmail.com>
2019-05-01 22:15:59 -0700

    Update Statistics.js
    Setting up a layout for Statistics.js

--- jour-www/src/components/Statistics/Statistics.js
+++ jour-www/src/components/Statistics/Statistics.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import './Statistics.css';
+import { connect } from 'react-redux';
 
 class Statistics extends Component {
   render() {
     return (
       <div className="Statistics">
         Statistics
+
+
+          <div className="dayCounter">
+          Days Signed Up:
+          </div>
+
+          <div className="entryCounter">
+          Entries:
+          </div>
+
+          <div className="emoteCounter">
+          Days Feeling Happy:
+          Days Feeling Sad:
+          Days Feeling Angry:
+          Days Feeling Anxious:
+          Days Feeling Confident:
+          Days Feeling Nostalgic:
+          </div>
+
+          <div className="tasksCompleted">
+          Tasks Completed:
+          </div>
+
+
+
+
       </div>
     );
   }
